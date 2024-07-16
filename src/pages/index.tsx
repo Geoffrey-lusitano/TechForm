6c8ff3ccb94a751adc9f7b452a530050eb82a59b
geoffrey lusitano <g.bl@outlook.fr>
2024-07-16 11:02:40 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+    <Seo title="TechForm" description="formations informatiques"/>
+    <h1>Bienvenue</h1>
+
+    </>
+  );
+}
